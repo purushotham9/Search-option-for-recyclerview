8471a7f575222321b0210f08d036ef5a63fc4779
Nanigire <nanigire@gmail.com>
2018-09-21 15:58:30 +0530

    search option in recyclerview using volley library

---
+++ app/src/main/java/com/example/kvanamacair4/volleylibrary/Contact.java
@@ -0,0 +1,24 @@
+package com.example.kvanamacair4.volleylibrary;
+
+public class Contact {
+
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+}
